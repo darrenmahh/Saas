27e9a7c23fcf7f39238e1b85b476a74580fddc25
Darren Ma <9537267+darren-ma@user.noreply.gitee.com>
2025-09-09 22:57:59 +0800

    feat:添加不脱敏接口

--- admin/src/main/java/org/getoffer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/org/getoffer/shortlink/admin/controller/UserController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.getoffer.shortlink.admin.common.convention.result.Result;
 import org.getoffer.shortlink.admin.common.convention.result.Results;
 import org.getoffer.shortlink.admin.common.enums.UserErrorCodeEnum;
+import org.getoffer.shortlink.admin.dto.resq.UserRespActualDTO;
 import org.getoffer.shortlink.admin.dto.resq.UserRespDTO;
 import org.getoffer.shortlink.admin.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,14 +17,23 @@ public class UserController {
 
     private final UserService userService;
 
+    /**
+     * 根据用户名获取用户信息，包含脱敏后的手机号等信息
+     * @param username
+     * @return
+     */
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
-        UserRespDTO result = userService.getUserByUsername(username);
-        if (result == null) {
-            return new Result<UserRespDTO>().setCode(UserErrorCodeEnum.User_NULL.code()).setMessage(UserErrorCodeEnum.User_NULL.message());
-        } else {
-            return Results.success(result);
-        }
+        return Results.success(userService.getUserByUsername(username));
+    }
 
+    /**
+     * 获取用户的真实信息，包含手机号等敏感信息
+     * @param username
+     * @return
+     */
+    @GetMapping("/api/shortlink/v1/actual/user/{username}")
+    public Result<UserRespActualDTO> getUserActualByUsername(@PathVariable("username") String username) {
+        return Results.success(userService.getUserActualByUsername(username));
     }
 }
