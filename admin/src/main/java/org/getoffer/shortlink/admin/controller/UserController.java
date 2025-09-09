04f347f04c090c41ff92ba6d36160c30d1662abb
Darren Ma <9537267+darren-ma@user.noreply.gitee.com>
2025-09-09 21:36:06 +0800

    添加全局错误码

--- admin/src/main/java/org/getoffer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/org/getoffer/shortlink/admin/controller/UserController.java
@@ -1,6 +1,8 @@
 package org.getoffer.shortlink.admin.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.getoffer.shortlink.admin.common.convention.result.Result;
+import org.getoffer.shortlink.admin.common.enums.UserErrorCodeEnum;
 import org.getoffer.shortlink.admin.dto.resq.UserRespDTO;
 import org.getoffer.shortlink.admin.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +16,13 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/api/shortlink/v1/user/{username}")
-    public UserRespDTO getUserByUsername(@PathVariable("username") String username) {
-        return userService.getUserByUsername(username);
+    public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
+        UserRespDTO result = userService.getUserByUsername(username);
+        if (result == null) {
+            return new Result<UserRespDTO>().setCode(UserErrorCodeEnum.User_NULL.code()).setMessage(UserErrorCodeEnum.User_NULL.message());
+        } else {
+            return new Result<UserRespDTO>().setCode("0").setData(userService.getUserByUsername(username));
+        }
+
     }
 }
