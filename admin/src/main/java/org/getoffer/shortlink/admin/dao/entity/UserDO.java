59be9e2210b989e43a342cfcc64e6e2923d55e81
Darren Ma <9537267+darren-ma@user.noreply.gitee.com>
2025-09-11 23:34:33 +0800

    feat:为用户实体添加注释

--- admin/src/main/java/org/getoffer/shortlink/admin/dao/entity/UserDO.java
+++ admin/src/main/java/org/getoffer/shortlink/admin/dao/entity/UserDO.java
@@ -8,23 +8,53 @@ import java.time.LocalDateTime;
 @Data
 @TableName("t_user")
 public class UserDO {
+    /**
+     * 用户ID
+     */
     private Long id;
 
+    /**
+     * 用户名
+     */
     private String username;
 
+    /**
+     * 密码
+     */
     private String password;
 
+    /**
+     * 真实姓名
+     */
     private String realName;
 
+    /**
+     * 手机号
+     */
     private String phone;
 
+    /**
+     * 邮箱
+     */
     private String mail;
 
+    /**
+     * 盐值
+     */
     private Long deletionTime;
 
+    /**
+     * 创建时间
+     */
     private LocalDateTime createTime;
 
+    /**
+     * 更新时间
+     */
     private LocalDateTime updateTime;
 
+    /**
+     * 删除标志 0-未删除 1-已删除
+     */
     private Integer delFlag;
 }
