b49e36eac89bbd62c50ade1ea5f952fe9bd9722e
Darren Ma <9537267+darren-ma@user.noreply.gitee.com>
2025-09-21 14:39:46 +0800

    feat:添加保存分组注释

--- admin/src/main/java/org/getoffer/shortlink/admin/service/GroupService.java
+++ admin/src/main/java/org/getoffer/shortlink/admin/service/GroupService.java
@@ -4,5 +4,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import org.getoffer.shortlink.admin.dao.entity.GroupDO;
 
 public interface GroupService extends IService<GroupDO> {
+    /**
+     * 新增短链接分子
+     * @param groupName 分组名
+     */
     void saveGroup(String groupName);
 }
