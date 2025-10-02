d257fb76b0970aa0689d56642d12efa62fafe9c0
Darren Ma <9537267+darren-ma@user.noreply.gitee.com>
2025-10-02 17:27:21 +0800

    feat:修改插入数据库短链接实体为链式调用及添加布隆误判解决逻辑

--- project/src/main/java/org/getoffer/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/org/getoffer/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -1,28 +1,63 @@
 package org.getoffer.shortlink.project.service.impl;
 
-import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.text.StrBuilder;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.getoffer.shortlink.project.common.convention.exception.ServiceException;
 import org.getoffer.shortlink.project.dao.entity.ShortLinkDO;
 import org.getoffer.shortlink.project.dao.mapper.ShortLinkMapper;
 import org.getoffer.shortlink.project.dto.req.ShortLinkCreateReqDTO;
 import org.getoffer.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
 import org.getoffer.shortlink.project.service.ShortLinkService;
 import org.getoffer.shortlink.project.toolkit.HashUtil;
+import org.redisson.api.RBloomFilter;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLinkDO> implements ShortLinkService {
+
+
+    private final RBloomFilter<String> shortUriCreateCachePenetrationBloomFilter;
 
     @Override
     public ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO reqDTO) {
+
         String shortLinkSuffix = generateSuffix(reqDTO);
-        ShortLinkDO shortLinkDO = BeanUtil.toBean(reqDTO, ShortLinkDO.class);
-        shortLinkDO.setShortUri(shortLinkSuffix);
-        shortLinkDO.setEnableStatus(0);
-        shortLinkDO.setFullShortUrl(reqDTO.getDomain() + "/" + shortLinkSuffix);
-        baseMapper.insert(shortLinkDO);
+        String fullShortUrl = StrBuilder
+                .create(reqDTO.getDomain())
+                .append("/")
+                .append(shortLinkSuffix)
+                .toString();
+
+        ShortLinkDO shortLinkDO =  ShortLinkDO.builder()
+                .domain(reqDTO.getDomain())
+                .originUrl(reqDTO.getOriginUrl())
+                .gid(reqDTO.getGid())
+                .createdType(reqDTO.getCreatedType())
+                .validDateType(reqDTO.getValidDateType())
+                .validDate(reqDTO.getValidDate())
+                .describe(reqDTO.getDescribe())
+                .shortUri(shortLinkSuffix)
+                .enableStatus(0)
+                .fullShortUrl(fullShortUrl)
+                .build();
+        try {
+            baseMapper.insert(shortLinkDO);
+        } catch (Exception e) {
+            LambdaQueryWrapper<ShortLinkDO> eq = Wrappers.lambdaQuery(ShortLinkDO.class)
+                    .eq(ShortLinkDO::getFullShortUrl, shortLinkDO.getFullShortUrl());
+            ShortLinkDO hasShortLink = baseMapper.selectOne(eq);
+            if (hasShortLink != null) {
+                log.warn("短链接 {} 重复创建",fullShortUrl);
+                throw new ServiceException("短链接重复创建");
+            }
+        }
+        shortUriCreateCachePenetrationBloomFilter.add(fullShortUrl);
         return ShortLinkCreateRespDTO.builder()
                 .fullShortUrl(shortLinkDO.getFullShortUrl())
                 .originUrl(reqDTO.getOriginUrl())
@@ -31,7 +66,25 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
     }
 
     private String generateSuffix(ShortLinkCreateReqDTO reqDTO) {
-        String originUrl = reqDTO.getOriginUrl();
-        return HashUtil.hashToBase62(originUrl);
+        int customGenerateCount = 0;
+        String shortUri;
+        while (true) {
+            if (customGenerateCount > 10) {
+                throw new ServiceException("短链接频繁生成，请稍后再试。");
+            }
+            // 从传入的实体中查找原始uri
+            String originUri = reqDTO.getOriginUrl();
+            System.out.println("原始完整链接" + originUri);
+            // 为原始链接创建唯一的短链接
+            originUri += System.currentTimeMillis();
+            System.out.println("加上完整链接之后的" + originUri);
+            shortUri = HashUtil.hashToBase62(originUri);
+
+            if (!shortUriCreateCachePenetrationBloomFilter.contains(reqDTO.getDomain() + "/" + shortUri)) {
+                break;
+            }
+            customGenerateCount++;
+        }
+        return shortUri;
     }
 }
