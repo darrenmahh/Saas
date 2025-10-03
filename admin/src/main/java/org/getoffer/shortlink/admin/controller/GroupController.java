798f03801f9cf2f40e280ee124733843e7f274ce
Darren Ma <9537267+darren-ma@user.noreply.gitee.com>
2025-10-03 19:55:14 +0800

    optimize：优化分组链接

--- admin/src/main/java/org/getoffer/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/org/getoffer/shortlink/admin/controller/GroupController.java
@@ -13,34 +13,35 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/short-link/admin")
 @RequiredArgsConstructor
 public class GroupController {
     private final GroupService groupService;
 
-    @PostMapping("/api/short-link/v1/group")
+    @PostMapping("/v1/group")
     public Result<Void> save(@RequestBody ShortLinkGroupSaveReqDTO reqDTO) {
         groupService.saveGroup(reqDTO.getName());
         return Results.success();
     }
 
-    @GetMapping("/api/short-link/v1/group")
+    @GetMapping("/v1/group")
     public Result<List<ShortLinkGroupSaveRespDTO>> listGroup() {
         return Results.success(groupService.listGroup());
     }
 
-    @PutMapping("/api/short-link/v1/group")
+    @PutMapping("/v1/group")
     public Result<Void> updateGroup(@RequestBody ShortLinkGroupUpdateReqDTO reqDTO) {
         groupService.update(reqDTO);
         return Results.success();
     }
 
-    @DeleteMapping("/api/short-link/v1/group")
+    @DeleteMapping("/v1/group")
     public Result<Void> updateGroup(@RequestParam String gid) {
         groupService.deleteGroup(gid);
         return  Results.success();
     }
 
-    @PostMapping("/api/short-link/v1/group/sort")
+    @PostMapping("/v1/group/sort")
     public Result<Void> sortGroup(@RequestBody List<ShortLinkGroupSortReqDTO> reqDTO) {
         groupService.sortGroup(reqDTO);
         return Results.success();
