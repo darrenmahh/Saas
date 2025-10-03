eba57d1e709d82f0b243b4dd5b754e2afce7a91d
Darren Ma <9537267+darren-ma@user.noreply.gitee.com>
2025-10-03 19:54:00 +0800

    feat:添加短链接分页请求及返回实体

---
+++ admin/src/main/java/org/getoffer/shortlink/admin/remote/dto/req/ShortLinkPageReqDTO.java
@@ -0,0 +1,11 @@
+package org.getoffer.shortlink.admin.remote.dto.req;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.Data;
+@Data
+public class ShortLinkPageReqDTO extends Page {
+    /**
+     * 分组标识
+     */
+    private String gid;
+}
